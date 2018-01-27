a1047dd9993f0bd47745999c3c122f40a8667e18
rog-works <rog_works@yahoo.co.jp>
2018-01-27 14:23:14 +0900

    feat: add web contents

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+	//phpinfo();
+	$db = new PDO('mysql:host=db;dbname=test;charset=UTF-8', 'root', '');
+	$ret = $db->query('select * from user');
+	$rows = $ret->fetchAll(PDO::FETCH_ASSOC);
+	foreach ($rows as $index => $row) {
+		echo "$index: -----------------";
+		print_r($row);
+	}
+	//echo file_get_contents(realpath(__DIR__ . '/index.html'));
