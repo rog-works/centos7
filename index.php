95ea2268d7f8a2166599b7e3c7eb4b311934b357
rog-works <rog_works@yahoo.co.jp>
2018-02-07 00:23:54 +0900

    fix: routing

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+	define('INDEX1', 'index1');
+	require('/var/www/public/index.php');
--- public/index.php
+++ public/index.php
@@ -1,12 +1,3 @@
 <?php
-	//phpinfo();
-	$db = new PDO('mysql:host=db;dbname=test;charset=UTF-8', 'root', '');
-	$ret = $db->query('select * from user');
-	$rows = $ret->fetchAll(PDO::FETCH_ASSOC);
-	foreach ($rows as $index => $row) {
-		echo "$index: -----------------";
-		print_r($row);
-	}
-	echo "query string --------------";
-	print_r($_SERVER);
-	//echo file_get_contents(realpath(__DIR__ . '/index.html'));
+	define('INDEX2', 'index2');
+	require('/var/www/src/controller.php');
