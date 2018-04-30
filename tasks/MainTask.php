23e15167dcca28ce9c47876c181c637ca8006889
rog-works <rog_works@yahoo.co.jp>
2018-04-30 16:28:00 +0900

    feat: impl sub action

--- tasks/MainTask.php
+++ tasks/MainTask.php
@@ -6,4 +6,8 @@ class MainTask extends \Phalcon\Cli\Task {
 	public function mainAction() {
 		echo 'Hello Task!';
 	}
+
+	public function subAction(array $args) {
+		echo 'args: ' . implode(',', $args);
+	}
 }
