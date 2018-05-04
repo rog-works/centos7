3a3d80389842b40fc54d8918a1ac79c10875baed
rog-works <rog_works@yahoo.co.jp>
2018-05-04 18:35:03 +0900

    refactor: guard not exists

--- app/di/Injector.php
+++ app/di/Injector.php
@@ -2,6 +2,8 @@
 
 namespace App\Di;
 
+use App\Exception\Runtime as RuntimeException;
+
 class Injector {
 	public static function resolve(string $class, array $methods) {
 		$instance = self::create($class, $methods['__construct'] ?? []);
@@ -14,10 +16,16 @@ class Injector {
 	}
 
 	private static function create(string $class, array $args) {
+		if (!class_exists($class)) {
+			throw new RuntimeException("class not found. class = {$class}");
+		}
 		return new $class(...array_values($args));
 	}
 
 	private static function invoke($instance, string $method, array $args) {
+		if (!method_exists($instance, $method)) {
+			throw new RuntimeException(sprintf('method not found. method = %s::%s', get_class($instance), $method));
+		}
 		$instance->{$method}(...array_values($args));
 	}
 }
